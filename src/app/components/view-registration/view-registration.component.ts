c0aeb7ab8afc5bbe576c1fa2945f9e6105c2aec0
muhammeta7 <muhammeta7@gmail.com>
2020-04-07 01:09:00 -0400

    angular project

---
+++ src/app/components/view-registration/view-registration.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { BikeService } from 'src/app/services/bike.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-view-registration',
+  templateUrl: './view-registration.component.html',
+  styleUrls: ['./view-registration.component.scss']
+})
+export class ViewRegistrationComponent implements OnInit {
+
+  public bikeReg;
+
+  constructor(private bikeService: BikeService, private route: ActivatedRoute) { }
+
+  ngOnInit(){
+    this.getBikeReg(this.route.snapshot.params.id);
+  }
+
+  getBikeReg(id:number){
+    this.bikeService.getBike(id).subscribe(
+      data => {
+        this.bikeReg = data;
+      },
+      err => console.log(err),
+      () => console.log('bikes loaded')
+    );
+  }
+
+}
